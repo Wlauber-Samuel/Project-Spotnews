f353522998f9b8e161d47c2de863a011c6036b74
Wlauber <wlaubersmuel@gmail.com>
2023-11-29 18:24:07 -0300

    requisito 03 concluído

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -6,6 +7,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def validate_title(title):
+    words = title.split()
+    if len(words) < 2:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
 
 
 class User(models.Model):
@@ -17,13 +24,14 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
-# class News(models.Model):
-#     title = models.CharField(max_length=200, unique=True, blank=False)
-#     content = models.TextField(blank=False)
-#     author = models.CharField(max_length=200, blank=False)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     image = models.ImageField(upload_to='img/', blank=True)
-#     categories = models.ManyToManyField(Category, related_name='news')
 
-#     def __str__(self):
-#         return self.title
+class News(models.Model):
+    title = models.CharField(max_length=200, validators=[validate_title])
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    image = models.ImageField(upload_to="img/", blank=True)
+    categories = models.ManyToManyField("Category")
+
+    def __str__(self):
+        return self.title
